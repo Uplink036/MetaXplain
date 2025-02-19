0c7a0a2b75ae5dad2f08f47a4d69be51e4f33b44
Uplink036 <oliversjodin02@gmail.com>
2025-02-19 10:40:27 +0000

    Runable train.py

--- containers/nn/run.sh
+++ containers/nn/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # pytest -l -p no:cacheprovider
+
+python3 train.py
--- containers/nn/train.py
+++ containers/nn/train.py
@@ -2,16 +2,40 @@ import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from data import batchLoader
 from neural_network import NeuralNetwork
+import torch.optim as optim
+
 
 if __name__ == "__main__":
     device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
     print(f"Using {device} device")
-    model = NeuralNetwork().to(device)
-    print(model)
+    net = NeuralNetwork().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    loader = batchLoader()
+    batch_size = loader.batch_size
 
-    X = torch.rand(1, 28, 28, device=device)
-    logits = model(X)
-    pred_probab = nn.Softmax(dim=1)(logits)
-    y_pred = pred_probab.argmax(1)
-    print(f"Predicted class: {y_pred}")
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for iteration in range(0, loader.get_number_of_batches()):
+            # get the inputs; data is a list of [inputs, labels]
+            labels, inputs = loader.batch()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if iteration % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {iteration + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
