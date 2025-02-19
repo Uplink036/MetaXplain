3f9b49ff20cf824fdc342843ff3a67c1dc7855ef
Uplink036 <oliversjodin02@gmail.com>
2025-02-19 09:00:03 +0000

    Add batchLoader

---
+++ containers/nn/data.py
@@ -0,0 +1,36 @@
+import os 
+import pymongo
+import dotenv
+
+class batchLoader():
+    def __init__(self):
+        self.batch_nr = 0
+        self.batch_size = 32
+        self.client = None
+        self.database = None
+        self.collection = None
+        self._init_db()
+
+    def _init_db(self):
+        try:
+            database_url = os.environ.get("DATABASE_URL")
+            client = pymongo.MongoClient(database_url)
+            client.server_info()
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print(err)
+            print("Are you sure your database is on and this can reach it?")
+            raise ConnectionError
+        self.client = client
+        self.database = client["MNIST"]
+        self.collection = self.database["dataset"]
+    def batch(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def get_number_of_batches(self, batch_size = None):
+        if batch_size is None:
+            batch_size = self.batch_size
+
+        return self.collection.count_documents({}) / batch_size
