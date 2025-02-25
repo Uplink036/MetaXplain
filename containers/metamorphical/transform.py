3f07766e58a6745bb0e1307b3660553fe9c9af40
Uplink036 <oliversjodin02@gmail.com>
2025-02-25 13:38:06 +0000

    Imrpove transformation

--- containers/metamorphical/transform.py
+++ containers/metamorphical/transform.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
     for data in dataset.find({"status":"test"}):
         image = data["image"]
         np_image = np.reshape(image, (28, 28))
-        for angle in (-45, 45, 15):
-            random_noise =  random.normalvariate(0, 7)
-            rotated_image = rotate(image, angle+random_noise)
+        for angle in range(-15, 15+5, 5):
+            random_noise =  random.normalvariate(0, 4)
+            rotated_image = rotate(np_image, angle+random_noise)
             meta_dict = {
                 "label": data["label"],
                 "size": "28x28",
-                "image": rotated_image.flatten(),
-                "status": "rotation"
+                "image": rotated_image.flatten().tolist(),
+                "status": "rotation",
+                "angle": angle+random_noise
             }
             dataset.upload(meta_dict)
